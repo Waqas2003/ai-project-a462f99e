cffe98d55b61c1f008ca7dbd94b2d52dc09fb4f6
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-09 21:27:18 +0500

    Add src\components\Footer.jsx

---
+++ src\components\Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-200 h-24 flex justify-center items-center">
+      <p className="text-gray-600">Copyright 2023 Travel Management System</p>
+    </footer>
+  );
+};
+
+export default Footer;
