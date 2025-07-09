e394fb557a70164dcab51d4641307efaa2e1a395
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-09 21:27:20 +0500

    Add src\components\Hero.jsx

---
+++ src\components\Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <section className="bg-cover bg-center h-screen flex justify-center items-center">
+      <h1 className="text-4xl font-bold text-white">Travel Management System</h1>
+    </section>
+  );
+};
+
+export default Hero;
